1a80edf75173a3b82aa779a94fadc774a8179263
Larry <lauris.skraucis@gmail.com>
2021-06-03 16:17:33 +0200

    Moved hero to seperate component

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -3,9 +3,10 @@ import { SharedModule } from '../../shared/shared.module';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, HeroComponent],
   imports: [SharedModule, HomeRoutingModule],
 })
 export class HomeModule {}
